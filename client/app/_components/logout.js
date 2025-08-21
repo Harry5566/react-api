a26cc6499909fe6d47ca9e94e1d0d55a3532cb91
Ben Chen <idben.chen@gmail.com>
2025-08-21 10:51:38 +0800

    開始接 server API login

--- client/app/_components/logout.js
+++ client/app/_components/logout.js
@@ -1,10 +1,35 @@
 "use client";
 
-export default function Logout(){
-  return(
+import { useAuth } from "@/hooks/use-auth";
+import { useState } from "react";
+
+export default function Logout() {
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+  const {login} = useAuth();
+
+  const onclick = ()=>{
+    login(account, password);
+  };
+
+  return (
     <>
       <h1>登出狀態</h1>
-      <p>預計要放表單</p>
+      <div className="input-group mb-2">
+        <span className="input-group-text">帳號</span>
+        <input type="text" name="account" className="form-control"
+          value={account} onChange={(e)=>{setAccount(e.target.value)}}
+        />
+      </div>
+      <div className="input-group mb-2">
+        <span className="input-group-text">密碼</span>
+        <input type="password" name="password" className="form-control"
+          value={password} onChange={(e)=>{setPassword(e.target.value)}}
+        />
+      </div>
+      <div className="d-flex">
+        <div className="btn btn-primary ms-auto btn-login" onClick={onclick}>送出</div>
+      </div>
     </>
-  )
+  );
 }
--- client/hooks/use-auth.js
+++ client/hooks/use-auth.js
@@ -1,15 +1,42 @@
 "use client";
 
 import {useContext, createContext, useState} from "react";
+import jwt from "jsonwebtoken";
 
 const AuthContext = createContext(null)
 AuthContext.displayName = "AuthContext";
+const appKey = "reactLoginToken";
 
 export function AuthProvider({children}){
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null);
+
+  const login = async (account, password)=>{
+    console.log(`在 use-auth 中, ${account}, ${password}`);
+    const API = "http://localhost:3005/api/users/login";
+    const formData = new FormData();
+    formData.append("account", account);
+    formData.append("password", password);
+    try{
+      const res = await fetch(API, {
+        method: "POST",
+        body: formData
+      });
+      const result = await res.json();
+      console.log(result);
+      
+      if(result.status == "success"){
+        console.log("成功");
+        
+      }else{
+        console.log("失敗");
+      }
+    }catch(error){
+      console.log(error);
+    }
+  };
 
   return(
-    <AuthContext.Provider value={{user}}>
+    <AuthContext.Provider value={{user, login}}>
       {children}
     </AuthContext.Provider>
   )
