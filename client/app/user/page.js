93c8a9b7ded0f033820abf9e7c2205c5d02fff65
Ben Chen <idben.chen@gmail.com>
2025-08-21 16:17:42 +0800

    使用者列表的API使用

--- client/app/user/page.js
+++ client/app/user/page.js
@@ -1,12 +1,60 @@
 "use client";
 
+import { useAuth } from "@/hooks/use-auth";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
-export default function UserPage(){
-  return(
+export default function UserPage() {
+  const {list, users} = useAuth();
+
+  useEffect(()=>{
+    list();
+  }, [list]);
+
+  if(!users.length){
+    return (
+      <div className="container py-3">
+        <h1>使用者列表頁</h1>
+        <h2>沒有使用者資訊</h2>
+        {/* <div className="btn btn-primary me-1" onClick={list}>取得使用者列表</div> */}
+        <Link className="btn btn-primary" href="/">
+          回首頁
+        </Link>
+      </div>
+    );
+  }
+  
+  return (
     <div className="container py-3">
       <h1>使用者列表頁</h1>
-      <Link className="btn btn-primary" href="/">回首頁</Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">account</th>
+            <th scope="col">mail</th>
+            <th scope="col">head</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index)=>(
+            <tr key={`user${index}`}>
+              <th scope="row">{index+1}</th>
+              <td>{user.accout}</td>
+              <td>{user.mail}</td>
+              <td>
+                <Image src={user.head} width={100} height={150} style={{objectFit: "contain"}} alt="" />
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+      {/* <div className="btn btn-primary me-1" onClick={list}>取得使用者列表</div> */}
+      <Link className="btn btn-primary" href="/">
+        回首頁
+      </Link>
     </div>
   );
 }
--- client/hooks/use-auth.js
+++ client/hooks/use-auth.js
@@ -1,14 +1,15 @@
 "use client";
 
 import { usePathname, useRouter } from "next/navigation";
-import {useContext, createContext, useState, useEffect} from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 AuthContext.displayName = "AuthContext";
 const appKey = "reactLoginToken";
 
-export function AuthProvider({children}){
+export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const router = useRouter();
@@ -16,108 +17,126 @@ export function AuthProvider({children}){
   const loginRoute = "/user/login";
   const protectedRoutes = ["/user"];
 
-  const login = async (account, password)=>{
+  const login = async (account, password) => {
     console.log(`在 use-auth 中, ${account}, ${password}`);
     const API = "http://localhost:3005/api/users/login";
     const formData = new FormData();
     formData.append("account", account);
     formData.append("password", password);
-    try{
+    try {
       const res = await fetch(API, {
         method: "POST",
-        body: formData
+        body: formData,
       });
       const result = await res.json();
       console.log(result);
-      
-      if(result.status == "success"){
+
+      if (result.status == "success") {
         const token = result.data.token;
         setUser(result.data.user);
         localStorage.setItem(appKey, token);
-      }else{
+      } else {
         alert(result.message);
       }
-    }catch(error){
+    } catch (error) {
       console.log(error);
     }
   };
 
-  const logout = async ()=>{
-    console.log("logout");
+  const logout = async () => {
     const API = "http://localhost:3005/api/users/logout";
     const token = localStorage.getItem(appKey);
-    try{
-      if(!token) throw new Error("Token 不存在");
+    try {
+      if (!token) throw new Error("Token 不存在");
       const res = await fetch(API, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       const result = await res.json();
-      if(result.status == "success"){
+      if (result.status == "success") {
         const token = result.data;
         setUser(null);
         // localStorage.setItem(appKey, token);
         localStorage.removeItem(appKey);
-      }else{
+      } else {
         // alert(result.message);
         throw new Error(result.message);
       }
-    }catch(error){
+    } catch (error) {
       console.log(`解析 token 失敗: ${error.message}`);
       setUser(null);
       localStorage.removeItem(appKey);
       alert(error.message);
     }
-  }
+  };
 
-  useEffect(()=>{
-    if(!isLoading && !user && protectedRoutes.includes(pathname)){
+  const list = async () => {
+    const API = "http://localhost:3005/api/users";
+    try {
+      const res = await fetch(API);
+      const result = await res.json();
+      
+      
+      if (result.status == "success") {
+        setUsers(result.data);
+      } else {
+        throw new Error(result.message);
+      }
+    } catch (error) {
+      console.log(`使用者列表取得: ${error.message}`);
+      setUsers([]);
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading && !user && protectedRoutes.includes(pathname)) {
       router.replace(loginRoute); // 導頁
     }
   }, [isLoading, user, pathname]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const API = "http://localhost:3005/api/users/status";
     const token = localStorage.getItem(appKey);
-    if(!token){
+    if (!token) {
       setUser(null);
       setIsLoading(false);
       return;
     }
-    const checkToken = async ()=>{
-      try{
+    const checkToken = async () => {
+      try {
         const res = await fetch(API, {
           method: "POST",
           headers: {
-            Authorization: `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
         const result = await res.json();
-        if(result.status == "success"){
+        if (result.status == "success") {
           const token = result.data.token;
           setUser(result.data.user);
           localStorage.setItem(appKey, token);
           setIsLoading(false);
-        }else{
+        } else {
           // alert(result.message);
           setIsLoading(false);
         }
-      }catch(error){
+      } catch (error) {
         console.log(`解析 token 失敗: ${error.message}`);
         setUser(null);
         localStorage.removeItem(appKey);
       }
-    }
+    };
     checkToken();
   }, []);
 
-  return(
-    <AuthContext.Provider value={{user, login, logout, isLoading}}>
+  return (
+    <AuthContext.Provider value={{ user, login, logout, isLoading, list, users }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export const useAuth = () => useContext(AuthContext);
