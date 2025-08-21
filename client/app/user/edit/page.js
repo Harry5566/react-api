bc4cdfea5434f0618897996321515f340140f7f1
Ben Chen <idben.chen@gmail.com>
2025-08-21 14:11:13 +0800

    app router: user 的主架構

---
+++ client/app/user/edit/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+export default function UserEditPage(){
+  return(
+    <div className="container py-3">
+      <h1>編輯使用者</h1>
+      <Link className="btn btn-primary" href="/">回首頁</Link>
+    </div>
+  );
+}
